aeecfb2beac4d6c7dd1cdfdfccba62b8d26cca18
Martinerre <martin.analista@gmail.com>
2022-12-09 11:11:20 -0300

    actualizacion

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
 // import styles from "../Card.module.css";
 import {Link} from "react-router-dom"
+import characters from "../../data";
 
 export default function Card(props) {
+   
    return (
       <div className="char">
          <button className="btn2" onClick={props.onClose}></button>
